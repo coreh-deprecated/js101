52cd2c3cdedd700dad5c7ea790668bc23e016c06
Marco Aurélio <thecoreh@gmail.com>
2011-10-20 19:36:11 -0200

    Comment canvas example 1.

--- canvas/exemplo1/js/main.js
+++ canvas/exemplo1/js/main.js
@@ -1,16 +1,21 @@
-$(function() {  
+$(function() {
+    // Pegamos o canvas com jQuery
     var canvas = $('#c')[0]
+    
+    // Pegamos o contexto de desenho 2d
     var context = canvas.getContext('2d')
     
+    // Definimos cor e largura das linhas
     context.strokeStyle = 'blue'
     context.lineWidth = 3;
-    
-    context.fillStyle = 'white'
 
+    // Começamos um path novo
     context.beginPath()
     
-    context.moveTo(200, 25)
+    // Movemos o cursor e adicionamos uma linha
+    context.moveTo(200, 25)    
     context.lineTo(320, 375)
 
+    // Desenhamos o path
     context.stroke()
 })
